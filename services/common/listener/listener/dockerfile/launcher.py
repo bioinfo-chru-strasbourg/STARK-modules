0c09d9761f687675523f767143dc94e8f191ed2e
lebechec <antony.lebechec@chru-strasbourg.fr>
2020-10-25 16:51:37 +0100

    V20201025

--- services/common/listener/listener/dockerfile/launcher.py
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-def launch(run, serviceName, containersFile, montage, image, launchCommand, configFile):
-	return "Informations manquantes pour lancer l'analyse du microservice "+serviceName+". Vérifiez les paramètres."
