bc61aefb1dd9bb2408de119c68ea726012de348b
lavauxt <lavauxt@gmail.com>
2022-03-09 13:31:22 +0100

    Update launch and config files

--- services/structuralvariation/decon/listener/config/launcher.py
+++ services/structuralvariation/decon/listener/config/launcher.py
@@ -33,42 +33,43 @@ def createContainerFile(containersFile, run, containerName):
 # containersFile is a log file created by the launcher.py
 # configFile is the path to the config file module.conf containing also "image" and "launchCommand"
 
-
 # /home1/data/STARK/config/structuralvariation/decon/listener/decon.conf
 # configFile is the --config argument from listener.py, defined in the decon.env file
 # we get image and launchCommand from the decon.conf file
-# os.getenv('MICROSERVICE_MONTAGE') == DOCKER_STARK_MODULE_SUBMODULE_SERVICE_LISTENER_PARAM_JSON
-configFile = os.getenv('DOCKER_STARK_MODULE_SUBMODULE_SERVICE_LISTENER_PARAM_JSON')
-json_config = json.load(configFile)
-launchCommand = getDataFromJson(json_config)['services'][serviceName]['launch']
-image = getDataFromJson(json_config)['services'][serviceName]['images']
+# DOCKER_STARK_MODULE_SUBMODULE_SERVICE_LISTENER_PARAM_CONF
+# ${DOCKER_STARK_MODULE_SUBMODULE_SERVICE_LISTENER_INNER_FOLDER_CONFIG} = ${DOCKER_STARK_INNER_FOLDER_CONFIG}/${DOCKER_STARK_MODULE_NAME}/${DOCKER_STARK_MODULE_SUBMODULE_NAME}/${DOCKER_STARK_MODULE_SUBMODULE_SERVICE_LISTENER_NAME}
+
+configFile = os.getenv('DOCKER_STARK_MODULE_SUBMODULE_SERVICE_LISTENER_PARAM_CONF')
+#print(configFile)
+
+serviceName = os.getenv('DOCKER_STARK_MODULE_SUBMODULE_SERVICE_LISTENER_PARAM_MICROSERVICE_NAME')
+#print(serviceName)
+
+with open(configFile,'r') as jsonAnalysisFile:
+	json_config = json.load(jsonAnalysisFile)
+
+launchCommand = json_config['services'][serviceName]['launch']
+image = json_config['services'][serviceName]['image']
+
+#print(launchCommand)
+#print(image)
 
 # Add the default.yaml config to the cmd and specific yaml file depending on the APP and/or GROUP tag
 # Find the GROUP or AP tag and then add an GROUP.yaml or a APP.yaml to the cmd after --config
 # get group [4] and app [5] name from run
 # /stark/output/repository/group/app/run
 # run structure is  "/STARK/output/repository/group/app/run"
-try:
-	group_name =  run.split('/')[4]
-	app_name = run.split('/')[5]
-except IndexError: pass
 
-# Path of the yaml config file
-# (/home1/data)/STARK/config/structuralvariation/decon/cli/*.yaml
-# option is only for GROUP NAME
-yaml_path = os.getenv('DOCKER_STARK_MODULE_SUBMODULE_SERVICE_CLI_INNER_FOLDER_CONFIG')
+run = ""
+group_name = ""
+app_name = ""
 
-if group_name and yaml_path:
-	yaml_config_file = yaml_path+"/"+group_name+".yaml"	
-	if not os.path.exists(yaml_config_file) and os.path.getsize(yaml_config_file) == 0:
-		yaml_config_file = yaml_path+"/default.yaml"
-else:
-	yaml_config_file = ""
 
 
 # snakefile command options after --config should be separated by a space
 # to exclude sample = EXCLUDE_SAMPLE_LIST=["SGT2101772","SGT2101762"]
 # exemple : snakemake -s snakefile_decon -c1 --config run=/STARK/data/run EXCLUDESAMPLE_LIST=["SGT2101769","SGT2101772"] --configfile default.yaml
+
 
 def launch(run, serviceName, containersFile, montage, image, launchCommand, configFile):
 	containerName = serviceName+os.path.basename(run)
@@ -77,7 +78,25 @@ def launch(run, serviceName, containersFile, montage, image, launchCommand, conf
 	# docker-compose -f to specify an alternate compose file (default: docker-compose.yml) ; --env-file PATH to specify an alternate environment file
 	# we use docker-compose run to launch an analysis, the -f argument can be use to specify a specific servicename.docker-compose.yml
 	# we can pass arguements like a classic docker run
-	cmd = "docker-compose run --rm -f "+serviceName+"docker-compose.yml " +cli_service_name+" "+launchCommand+" --config DATA_DIR="+os.path.basename(run)+" --configfile "+yaml_config_file
+	# docker-compose -f test.docker-compose.yml  run  stark-module-structuralvariation-submodule-decon-service-listener
+	try:
+		group_name = run.split('/')[4]
+		app_name = run.split('/')[5]
+	except IndexError: pass
+	# Path of the yaml config file
+	# (/home1/data)/STARK/config/structuralvariation/decon/cli/*.yaml
+	# option is only for GROUP NAME
+	yaml_path = os.getenv('DOCKER_STARK_MODULE_SUBMODULE_SERVICE_CLI_INNER_FOLDER_CONFIG')
+	#print(yaml_path)
+	if group_name and yaml_path:
+		yaml_config_file = yaml_path+"/"+group_name+".yaml"	
+	if not os.path.exists(yaml_config_file) and os.path.getsize(yaml_config_file) == 0:
+		yaml_config_file = yaml_path+"/default.yaml"
+	else:
+		yaml_config_file = ""
+	# Construct the command
+	cmd = "docker-compose -f "+serviceName+"docker-compose.yml run -rm " +cli_service_name+" "+launchCommand+" --config DATA_DIR="+os.path.basename(run)+" --configfile "+yaml_config_file
+	# launch the cmd to the shell 
 	subprocess.call(cmd, shell = True)
 	# Create a log file
 	createContainerFile(containersFile, run, containerName)
--- services/structuralvariation/scramble/listener/config/launcher.py
+++ services/structuralvariation/scramble/listener/config/launcher.py
@@ -37,47 +37,58 @@ def createContainerFile(containersFile, run, containerName):
 # /home1/data/STARK/config/structuralvariation/decon/listener/decon.conf
 # configFile is the --config argument from listener.py, defined in the decon.env file
 # we get image and launchCommand from the decon.conf file
-# os.getenv('MICROSERVICE_MONTAGE') == DOCKER_STARK_MODULE_SUBMODULE_SERVICE_LISTENER_PARAM_JSON
+# ${DOCKER_STARK_MODULE_SUBMODULE_SERVICE_LISTENER_INNER_FOLDER_CONFIG} = ${DOCKER_STARK_INNER_FOLDER_CONFIG}/${DOCKER_STARK_MODULE_NAME}/${DOCKER_STARK_MODULE_SUBMODULE_NAME}/${DOCKER_STARK_MODULE_SUBMODULE_SERVICE_LISTENER_NAME}
+
 configFile = os.getenv('DOCKER_STARK_MODULE_SUBMODULE_SERVICE_LISTENER_PARAM_JSON')
-json_config = json.load(configFile)
-launchCommand = getDataFromJson(json_config)['services'][serviceName]['launch']
-image = getDataFromJson(json_config)['services'][serviceName]['images']
+
+print os.getenv('DOCKER_STARK_INNER_FOLDER_CONFIG')
+print os.getenv('DOCKER_STARK_MODULE_NAME')
+print os.getenv('DOCKER_STARK_MODULE_SUBMODULE_NAME')
+print os.getenv('DOCKER_STARK_MODULE_SUBMODULE_SERVICE_LISTENER_NAME')
+print(configFile)
+#json_config = json.load(configFile)
+#launchCommand = getDataFromJson(json_config)['services'][serviceName]['launch']
+#image = getDataFromJson(json_config)['services'][serviceName]['images']
 
 # Add the default.yaml config to the cmd and specific yaml file depending on the APP and/or GROUP tag
 # Find the GROUP or AP tag and then add an GROUP.yaml or a APP.yaml to the cmd after --config
 # get group [4] and app [5] name from run
 # /stark/output/repository/group/app/run
 # run structure is  "/STARK/output/repository/group/app/run"
-try:
-	group_name =  run.split('/')[4]
-	app_name = run.split('/')[5]
-except IndexError: pass
+#try:
+#	group_name =  run.split('/')[4]
+#	app_name = run.split('/')[5]
+#except IndexError: pass
 
 # Path of the yaml config file
 # (/home1/data)/STARK/config/structuralvariation/decon/cli/*.yaml
 # option is only for GROUP NAME
 yaml_path = os.getenv('DOCKER_STARK_MODULE_SUBMODULE_SERVICE_CLI_INNER_FOLDER_CONFIG')
+print(yaml_path)
+#if group_name and yaml_path:
+#	yaml_config_file = yaml_path+"/"+group_name+".yaml"	
+#	if not os.path.exists(yaml_config_file) and os.path.getsize(yaml_config_file) == 0:
+#		yaml_config_file = yaml_path+"/default.yaml"
+#else:
+#	yaml_config_file = ""
 
-if group_name and yaml_path:
-	yaml_config_file = yaml_path+"/"+group_name+".yaml"	
-	if not os.path.exists(yaml_config_file) and os.path.getsize(yaml_config_file) == 0:
-		yaml_config_file = yaml_path+"/default.yaml"
-else:
-	yaml_config_file = ""
-
-
+#print(yaml_config_file)
 # snakefile command options after --config should be separated by a space
 # to exclude sample = EXCLUDE_SAMPLE_LIST=["SGT2101772","SGT2101762"]
 # exemple : snakemake -s snakefile_decon -c1 --config run=/STARK/data/run EXCLUDESAMPLE_LIST=["SGT2101769","SGT2101772"] --configfile default.yaml
 
-def launch(run, serviceName, containersFile, montage, image, launchCommand, configFile):
-	containerName = serviceName+os.path.basename(run)
-	cli_service_name = os.getenv('DOCKER_STARK_MODULE_SUBMODULE_SERVICE_CLI_CONTAINER_NAME')
+
+def launch():
+	print("launch SCRAMBLE")
+#def launch(run, serviceName, containersFile, montage, image, launchCommand, configFile):
+#	containerName = serviceName+os.path.basename(run)
+#	cli_service_name = os.getenv('DOCKER_STARK_MODULE_SUBMODULE_SERVICE_CLI_CONTAINER_NAME')
 	# Should be stark-module-structuralvariation-submodule-decon-service-cli
 	# docker-compose -f to specify an alternate compose file (default: docker-compose.yml) ; --env-file PATH to specify an alternate environment file
 	# we use docker-compose run to launch an analysis, the -f argument can be use to specify a specific servicename.docker-compose.yml
 	# we can pass arguements like a classic docker run
-	cmd = "docker-compose run --rm -f "+serviceName+"docker-compose.yml " +cli_service_name+" "+launchCommand+" --config DATA_DIR="+os.path.basename(run)+" --configfile "+yaml_config_file
-	subprocess.call(cmd, shell = True)
+#	cmd = "docker-compose run --rm -f "+serviceName+"docker-compose.yml " +cli_service_name+" "+launchCommand+" --config DATA_DIR="+os.path.basename(run)+" --configfile "+yaml_config_file
+#	print(cmd)
+	#subprocess.call(cmd, shell = True)
 	# Create a log file
-	createContainerFile(containersFile, run, containerName)
+	#createContainerFile(containersFile, run, containerName)
