57019a9de0982cc9caaf205165f2b3b88dffa23e
Jean-Baptiste Lamouche <bioinfo.chru.strasbourg@gmail.com>
2022-03-22 10:00:54 +0100

    Fix annotsv launch without any variant condition, fix plot graphs and add copy for plot based on copyplot config set as True, minor fix

--- services/structuralvariation/canoes/cli/dockerfile/src/archives/plotCoverage_devjb.R
+++ services/structuralvariation/canoes/cli/dockerfile/src/archives/plotCoverage_devjb.R
@@ -48,7 +48,8 @@ PlotCoverage <- function(SAMPLE, INPUT, OUTPUT){
 	#geom_text(aes(6.4,-2,label = "median coverage x0.25", vjust = -1)) +
 	labs(title = "Boxplot coverage mean taget sample vs all(normalised)", subtitle = paste("[INFO] Target Sample: ", SAMPLE, "\n[INFO] Processed samples: ", sample_list, "\n[INFO] Warning: Without sexual chromosomes, Normalisation: For each sample and for each region the coverage mean is divide by the total coverage mean of the sample and multply by the total coverage mean of the target sample (values are divide by the median(Graph:median=0), in log2 )"), x="Genomic Region(chromosome:start-end)", y="Coverage(normalised)(log2)") +
 	theme(axis.text=element_text(size=7),axis.text.x = element_text(angle = 90, vjust=0.5, hjust=1), plot.title = element_text(size=9), plot.subtitle = element_text(size=6)) +
-	png(width=nrows, height=400)
+	#png(width=nrows, height=400)
+	png(type="cairo")
 	ggsave(OUTPUT,limitsize = FALSE)
 }
 
@@ -65,6 +66,7 @@ PlotCoverage2 <- function(SAMPLE, INPUT, OUTPUT){
 	geom_bar(data=my_data, stat="identity", fill="skyblue", aes(x=factor(Genomic_Region, levels=lab), y=Coverage_Mean_Sample)) +
 	labs(title = "Barplot coverage mean per base per region", subtitle = paste("[INFO] Target Sample: ", SAMPLE), x="Genomic Region(chromosome:start-end)", y="Coverage mean(bp)") +
 	theme(axis.text=element_text(size=7) ,axis.text.x = element_text(angle = 90, vjust=0.5, hjust=1), plot.title = element_text(size=9), plot.subtitle = element_text(size=6)) +
-	png(width=nrows, height=400)
+	#png(width=nrows, height=400)
+	png(type="cairo")
 	ggsave(OUTPUT,limitsize = FALSE)
 }
