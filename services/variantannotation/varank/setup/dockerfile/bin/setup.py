d7e313e7ae8e707128fa7bec4980df481801b1c7
elyanes <rauchmateusz@gmail.com>
2022-05-13 13:37:13 +0200

    Second commit

---
+++ services/variantannotation/varank/setup/dockerfile/bin/setup.py
@@ -0,0 +1,25 @@
+# !/usr/bin/python
+# !/bin/sh
+# !/usr/tcl/bin
+# -*- coding: utf-8 -*-
+"""
+###############################
+##							 ##
+##		VARANK ANALYSIS		 ##
+## need alamut-batch license ##
+##	Author : Mateusz RAUCH	 ##
+##							 ##
+###############################
+"""
+import os
+import shutil
+
+
+def main():
+    print("Hello")
+    print("Goodbye")
+
+
+if __name__ == "__main__":
+    print(os.environ["DOCKER_STARK_MODULE_SUBMODULE_SERVICE_SETUP_INNER_FOLDER_CONFIG"])
+    main()
