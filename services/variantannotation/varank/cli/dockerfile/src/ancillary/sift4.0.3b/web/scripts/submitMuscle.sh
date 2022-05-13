49f949a16450153b553baedaa4cd6b557a57e9ec
elyanes <rauchmateusz@gmail.com>
2022-05-13 12:27:26 +0200

    "setup setup"

--- services/variantannotation/varank/cli/dockerfile/src/ancillary/sift4.0.3b/web/scripts/submitMuscle.sh
+++
@@ -1,8 +0,0 @@
-
-# This program is licensed to you under the Fred
-# Hutchinos Cancer Research Center (FHCRC)
-# NONCOMMERICAL LICENSE.  A copy of the license may be found at
-# http://blocks.fhcrc.org/sift/license.html and should be attached
-# to this software.
-
-qsub -P 08010 -j y -V -cwd  -l "arch=lx*64"  /usr/local/projects/GOSII/pkumar/tmp/scripts/muscle.sh $1 $2
