3acba7bdb292a2829339731cf65e77d8f65e937e
elyanes <rauchmateusz@gmail.com>
2022-05-16 13:43:52 +0200

    Ended setup script

---
+++ services/variantannotation/varank/cli/dockerfile/bin/vrank_wrapper2.py
@@ -0,0 +1,23 @@
+# !/usr/bin/python
+# !/bin/sh
+# !/usr/tcl/bin
+# -*- coding: utf-8 -*-
+"""
+###############################
+##							 ##
+##		VARANK ANALYSIS		 ##
+## need alamut-batch license ##
+##	Author : Mateusz RAUCH	 ##
+##							 ##
+###############################
+"""
+import os
+import argparse
+import sys
+import subprocess
+import shutil
+import glob
+from datetime import datetime
+import re
+import pandas
+import logging as log
