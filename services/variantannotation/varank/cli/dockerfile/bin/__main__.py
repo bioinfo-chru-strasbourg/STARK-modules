9dbe25f6e58275a07178b3bac402a55452d78424
elyanes <rauchmateusz@gmail.com>
2022-05-18 17:18:46 +0200

    arg_parse & first functions

--- services/variantannotation/varank/cli/dockerfile/bin/__main__.py
+++ services/variantannotation/varank/cli/dockerfile/bin/__main__.py
@@ -13,74 +13,93 @@
 """
 
 import argparse
+import sys
+import os
 from commons import set_log_level
+from checker import absolute_dir_path
 
 
 def main(args):
+    original_umask = os.umask(0o000)
     set_log_level(args.verbosity)
-    if args.inputFormat.lower() == "decon":
-        raise ValueError(
-            "DECON is handled as a TSV conversion. Use 'tsv' as input format"
-        )
-
-    factory = ConverterFactory()
-    converter = factory.get_converter(
-        args.inputFormat.lower(), args.outputFormat.lower(), args.configFile
-    )
-
-    if args.inputFormat == "varank":
-        if args.coordConversionFile == "":
-            raise ValueError(
-                "Converting from a Varank file requires setting the --coordConversionFile argument to an existing file"
-            )
-        if not os.path.exists(args.coordConversionFile):
-            raise ValueError(
-                "coordConversionFile does not exist:" + args.coordConversionFile
-            )
-        converter.set_coord_conversion_file(args.coordConversionFile)
-
-    converter.convert(args.inputFile, args.outputFile)
 
 
 def parse_args():
+    """Main parser"""
+    main_parser = argparse.ArgumentParser(prog="varank")
 
-    parser = argparse.ArgumentParser(prog="VaRank")
-    group = parser.add_mutually_exclusive_group(required=False)
-    group.add_argument(
-        "-f",
-        "--varank_folder",
+    """Secondary parsers with global arguments"""
+    verbosity_parser = argparse.ArgumentParser(add_help=False)
+    verbosity_parser.add_argument(
+        "-v",
+        "--verbosity",
         type=str,
-        help="absolute path to the folder where you want to launch VaRank analysis",
+        default="info",
+        help="Verbosity level, DEBUG, INFO, WARNING, ERROR, CRITICAL. Default value is INFO",
     )
-    group.add_argument(
-        "-r",
-        "--run",
-        type=str,
-        help="path to STARK run for which you want to launch VaRank analysis",
-    )
-    group.add_argument(
-        "-s",
-        "--vcfsync",
-        help="Used to rsync all VCF files in the described STARK repository and defined pattern",
-        action="store_true",
-    )
-    parser.add_argument(
+    pattern_parser = argparse.ArgumentParser(add_help=False)
+    pattern_parser.add_argument(
         "-p",
         "--pattern",
         type=str,
-        nargs="+",
-        help="pattern describing which vcf files to synchronize in STARK folders, actual patterns : */STARK/*.reports/*.final.vcf.gz, */POOL/*.final.vcf.gz. You can use two patterns with space as separator",
-    )
-    parser.add_argument(
-        "-v", "--verbosity", type=str, default="info", help="Verbosity level"
+        default="*/STARK/*.reports/*.final.vcf.gz",
+        help="pattern describing which vcf files to synchronize in STARK folders, actual patterns : */STARK/*.reports/*.final.vcf.gz (default), */POOL/*.final.vcf.gz. You can use two patterns with space as separator",
     )
 
-    if "verbosity" not in args:
-        parser.print_help()
+    """Subparser definition"""
+    subparsers = main_parser.add_subparsers(help="sub-command help")
 
-    return parser.parse_args()
+    parser_folder = subparsers.add_parser(
+        "folder",
+        help="run VaRank on any folder you want, must containing VCFs and optional custom configfile",
+        parents=[verbosity_parser],
+    )
+    parser_folder.add_argument(
+        "-f",
+        "--folder",
+        required=True,
+        type=absolute_dir_path,
+        help="absolute path to the folder where you want to launch VaRank analysis",
+    )
+
+    parser_run = subparsers.add_parser(
+        "run",
+        help="run VaRank on STARK run repository",
+        parents=[verbosity_parser, pattern_parser],
+    )
+    parser_run.add_argument(
+        "-r",
+        "--run",
+        required=True,
+        type=absolute_dir_path,
+        help="absolute path to STARK run for which you want to launch VaRank analysis",
+    )
+
+    parser_sync = subparsers.add_parser(
+        "sync",
+        help="used to rsync all VCF files in the described STARK repository/archives and defined pattern",
+        parents=[verbosity_parser, pattern_parser],
+    )
+    sync_group = parser_sync.add_mutually_exclusive_group(required=True)
+    sync_group.add_argument(
+        "-sr",
+        "--syncrepo",
+        action="store_true",
+    )
+    sync_group.add_argument(
+        "-sa",
+        "--syncarchives",
+        action="store_true",
+    )
+
+    args = main_parser.parse_args()
+
+    if len(sys.argv) == 1:
+        main_parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    main(args)
 
 
 if __name__ == "__main__":
-    args = parse_args
-    main(args)
+    parse_args()
