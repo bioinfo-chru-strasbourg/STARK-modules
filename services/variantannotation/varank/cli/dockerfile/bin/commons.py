c470d42b1c79500076526419a8537dc05f79260b
elyanes <rauchmateusz@gmail.com>
2022-06-08 17:43:21 +0200

    logging

--- services/variantannotation/varank/cli/dockerfile/bin/commons.py
+++ services/variantannotation/varank/cli/dockerfile/bin/commons.py
@@ -1,18 +1,83 @@
 # -*- coding: utf-8 -*-
 import os
-import sys
-import subprocess
-import shutil
-import re
-import pandas
-import glob
 import logging as log
+from os.path import join as osj
+from threading import local
 import time
 
 default_pattern = "*/STARK/*.reports/*.final.vcf.gz"
 
 
-def set_log_level(verbosity):
+# def logger_infos(args, log_file, seconds_start):
+#     info_file = log_file.replace(".log", ".info")
+#     if not os.path.isfile(info_file):
+#         # command =
+#         time_seconds_start = time.time() + 7200
+#         local_time = time.localtime(time_seconds_start)
+#         actual_time = time.strftime("%a %b %d %H:%M:%S %Y", local_time)
+#         start = actual_time
+#         print(start)
+
+#         log = f"""
+#         """
+#         with open(info_file, "a") as write_file:
+#             write_file.write(log)
+
+#         return info_file, time_seconds_start
+
+#     else:
+#         time_seconds_end = time.time() + 7200
+#         local_time = time.localtime(time_seconds_end)
+#         actual_time = time.strftime("%a %b %d %H:%M:%S %Y", local_time)
+#         end = actual_time
+#         delta = time_seconds_end - seconds_start
+#         print(delta)
+
+
+def logger_header(log_file):
+    log = f"""#########################
+#     VaRank Wrapper    #
+#    Variants Ranking   #
+# Author: Mateusz Rauch #
+#########################
+
+####################
+#   Release: {os.environ["DOCKER_STARK_MODULE_SUBMODULE_SERVICE_CLI_RELEASE"]}   #
+####################
+
+"""
+    with open(log_file, "a") as write_file:
+        write_file.write(log)
+
+
+def set_log_level(args):
+    verbosity = args.verbosity
+    run = args.run
+    mode = args.mode
+
+    run_name = run.split("/")[-2]
+    run_application = run.split("/")[-3]
+    run_platform = run.split("/")[-4]
+
+    time_seconds = time.time() + 7200
+    local_time = time.localtime(time_seconds)
+    actual_time = time.strftime("%Y%m%d_%H%M%S", local_time)
+
+    if mode == "manual":
+        log_file = (
+            f"{actual_time}_{run_platform}_{run_application}_{run_name}_{mode}.log"
+        )
+    elif mode == "listener":
+        log_file = (
+            f"{actual_time}_{run_platform}_{run_application}_{run_name}_{mode}.log"
+        )
+
+    log_file = osj(
+        os.environ["DOCKER_MODULE_VARIANTANNOTATION_SUBMODULE_VARANK_FOLDER_SERVICES"],
+        "logs",
+        log_file,
+    )
+
     configs = {
         "debug": log.DEBUG,
         "info": log.INFO,
@@ -28,12 +93,15 @@ def set_log_level(verbosity):
             + configs.keys()
         )
     log.basicConfig(
-        filename="",
+        filename=log_file,
         filemode="a",
         format="%(asctime)s [%(levelname)s] %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
         level=configs[verbosity],
     )
+
+    # mylog = log.getLogger()
+    # print(mylog.handlers[0].baseFilename)
 
 
 if __name__ == "__main__":
--- services/variantannotation/varank/setup/dockerfile/bin/setup.py
+++ services/variantannotation/varank/setup/dockerfile/bin/setup.py
@@ -26,6 +26,7 @@ def main():
     alamut_licence()
     varank_configuration()
     varank_listener_configuration()
+    varank_services()
     alamutdb_checker()
 
 
@@ -57,6 +58,72 @@ def varank_listener_configuration():
     configuration_files = ["launcher.py", "varank.json"]
     for i in configuration_files:
         if_not_file_copy("listener", i)
+
+
+def varank_services():
+    main_folders = ["Archives", "listener", "logs"]
+    for i in main_folders:
+        if_not_folder_create(services_varank, i)
+
+    stark_module = osj(services_varank[:-6], "STARK.module")
+    stark_submodule = osj(services_varank, "STARK.module")
+
+    if not os.path.isfile(stark_module):
+        json = {
+            "code": "variantannotation",
+            "name": "Variant Annotation",
+            "fullname": "Variant Annotation Services",
+            "release": os.environ["DOCKER_STARK_MODULE_SUBMODULE_RELEASE"],
+            "available": "true",
+        }
+
+        with open(stark_module, "w") as write_file:
+            json.dump(json, write_file, indent=4)
+
+    if not os.path.isfile(stark_submodule):
+        json = {
+            "submodules": {
+                "varank": {
+                    "description": "Only VaRank annotation tool",
+                    "website": "https://www.lbgi.fr/VaRank/",
+                    "services": {
+                        "cli": {
+                            "code": "cli",
+                            "name": "VaRank cli",
+                            "type": "cli",
+                            "description": "A VaRank cli",
+                            "release": os.environ[
+                                "DOCKER_STARK_MODULE_SUBMODULE_SERVICE_CLI_RELEASE"
+                            ],
+                            "available": "true",
+                        },
+                        "listener": {
+                            "code": "listener",
+                            "name": "VaRank listener",
+                            "type": "listener",
+                            "description": "A VaRank listener service is started as a daemon, listening for new STARK analyzed run (new folder in output/repository) and well configured (STARKComplete.txt without VaRankRunning.txt or VaRankComplete.txt), and check triggers to launch or not analysis",
+                            "release": os.environ[
+                                "DOCKER_STARK_MODULE_SUBMODULE_SERVICE_LISTENER_RELEASE"
+                            ],
+                            "available": "true",
+                        },
+                        "setup": {
+                            "code": "setup",
+                            "name": "VaRank setup",
+                            "type": "setup",
+                            "description": "A VaRank setup, prepare all the folders and configurations files for VaRank to work. It will also download AlamutDB if not available",
+                            "release": os.environ[
+                                "DOCKER_STARK_MODULE_SUBMODULE_SERVICE_SETUP_RELEASE"
+                            ],
+                            "available": "true",
+                        },
+                    },
+                }
+            }
+        }
+
+        with open(stark_submodule, "w") as write_file:
+            json.dump(json, write_file, indent=4)
 
 
 def if_not_file_copy(inner_folder, inner_file):
@@ -144,5 +211,8 @@ def alamutdb_checker():
 if __name__ == "__main__":
     config_varank = os.environ[
         "DOCKER_STARK_MODULE_SUBMODULE_SERVICE_SETUP_INNER_FOLDER_CONFIG"
+    ]
+    services_varank = os.environ[
+        "DOCKER_MODULE_VARIANTANNOTATION_SUBMODULE_VARANK_FOLDER_SERVICES"
     ]
     main()
