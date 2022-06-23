324381875d766ef866f5008d759d9deadd2cb2b1
lavauxt <lavauxt@github.com>
2022-06-23 15:07:49 +0200

    Tsv converter

--- services/structuralvariation/decon/cli/dockerfile/src/vcf2tsv_converter.py
+++ services/structuralvariation/decon/cli/dockerfile/src/vcf2tsv_converter.py
@@ -1,5 +1,5 @@
 ##########################################################################
-# Vcf2Tsv Version:			1.0
+# Vcf2Tsv Version:			2.0
 # Description:				Script to convert vcf to a tsv file
 ##########################################################################
 
@@ -7,29 +7,25 @@
 
 # Authoring : Jean-baptiste LAMOUCHE
 
-# TL : Add a input and an output option, argparse
-
-
-################## Context ##################
-# type python Vcf2Tsv_converter.py -h for help
-# work only for mono sample vcfs
-####################################
+# TL	: Add a input and an output option, argparse
+# 		: Add uncompress function for vcf.gz
 
 import pandas as pd
 from tqdm import tqdm
 import subprocess
 import sys
 import numpy as np
+import shutil
+import gzip
 
-import argparse
-import doctest
 
-def parse_sample_field(dfVar):
+def _parse_sample_field(dfVar):
 	#############
 	### Parsing Sample Field in VCF
 	#############
 
 	dico = []
+	# dfTest = pd.Series(dfVar.TEM195660.values,index=dfVar.FORMAT).to_dict()
 	bad_annotation = []
 	sample_list = []
 
@@ -74,6 +70,66 @@ def parse_sample_field(dfVar):
 	return df_final
 
 
+def parse_sample_field(dfVar):
+	# UPDATE 21/06/2022
+	# handle multisample and keep information of sample field like <sample>_<field> for each annotations
+
+	#############
+	### Parsing Sample Field in VCF
+	#############
+
+	dico = []
+	# dfTest = pd.Series(dfVar.TEM195660.values,index=dfVar.FORMAT).to_dict()
+	bad_annotation = []
+	sample_list = []
+
+	# Parsing FORMAT field in VCF
+	# print("[#INFO] Parsing FORMAT field")
+	isample = list(dfVar.columns).index("FORMAT") + 1
+	# index: line where the caller identify an event somethings
+	for col in dfVar.columns[isample:]:
+		# print("#[INFO] " + col + "\n")
+		tmp_ = []
+		sample_list.append(col)
+		for i, row in tqdm(
+			dfVar.iterrows(),
+			total=dfVar.shape[0],
+			leave=True,
+			desc="#[INFO] SAMPLE " + col,
+		):
+			# print_progress_bar(i, len(dfVar.index)-1)
+			# print('\n')
+			# print(row['FORMAT'].split(':'), row['bwamem.VarScan_HUSTUMSOL.howard'].split(':'))
+			if len(row["FORMAT"].split(":")) != len(row[col].split(":")):
+				bad_annotation.append(pd.Series(row[:], index=dfVar.columns))
+				continue
+			else:
+				# If more than on sample
+				if len(dfVar.columns[isample:]) > 1:
+					index = [col + "_" + x for x in row["FORMAT"].split(":")]
+				else:
+					index = row["FORMAT"].split(":")
+				toadd = pd.Series(
+					row[col].split(":"),
+					index=index,
+				).to_dict()
+				# toadd.update({"caller":col})
+				tmp_.append(toadd)
+		dico.append(pd.DataFrame(tmp_))
+
+	dfSample = pd.concat(dico, axis=1)
+	df_bad_anno = pd.DataFrame(bad_annotation)
+	try:
+		df_final = dfVar.join(dfSample, how="inner")
+	except ValueError:
+		df_final = dfVar.join(dfSample, how="inner", lsuffix="_INFO", rsuffix="_SAMPLE")
+		print(
+			"WARNING some columns are present in both INFO and SAMPLE field, add _INFO and _SAMPLE suffix"
+		)
+	df_final.drop(columns=sample_list, inplace=True)
+	return df_final, df_bad_anno, sample_list
+
+
 def parse_info_field(dfVar):
 	"""
 	input: take a dataframe (from vcf)
@@ -95,7 +151,7 @@ def parse_info_field(dfVar):
 		dfVar.iterrows(), total=dfVar.shape[0], desc="#[INFO] INFO column split"
 	):
 		# print_progress_bar(i, len(dfVar.index)-1)
-		infoList.append([x.split("=") for x in elems["INFO"].split(";")])
+		infoList.append([x.split("=", 1) for x in elems["INFO"].split(";")])
 
 	# print("\n")
 
@@ -172,13 +228,27 @@ def preprocess_vcf(file):
 		df = pd.read_csv(
 			file, skiprows=skip[0], sep="\t", header=0, chunksize=1000, low_memory=False
 		)
-	else:
+	elif len(systemcall("grep -v '^#' " + file + " | wc -l")) <= 10000:
 		df = pd.read_csv(file, skiprows=skip[0], sep="\t", header=0)
+	else:
+		return pd.read_csv(colums=data["fields"])
 	return df
 
 
+def uncompress_file(compress, uncompress):
+	with gzip.open(compress, "r") as f_in, open(uncompress, "wb") as f_out:
+		shutil.copyfileobj(f_in, f_out)
+	return f_out
+
+
 def main(inputfile, outputfile):
-	df_variants = parse_sample_field(parse_info_field(preprocess_vcf(inputfile)))
+	if os.path.basename(inputfile).endswith('.vcf.gz'):
+		uncompress_file(inputfile, inputfile_vcf)
+	else
+		inputfile_vcf = inputfile
+	df_variants, _, _l = parse_sample_field(preprocess_vcf(inputfile_vcf))
+	tmp = vcf.split(".")[:-1]
+	out = ".".join(tmp) + ".tsv"
 	df_variants.to_csv(outputfile, sep="\t", header=True, index=False)
 
 
@@ -188,9 +258,10 @@ def myoptions():
 	*return options*
 	'''
 	parser = argparse.ArgumentParser()
-	parser.add_argument("-i", "--input", type = str, default = "", help = "Input vcf file", dest = 'inputfile')
+	parser.add_argument("-i", "--input", type = str, default = "", help = "Input vcf or vcf.gz file", dest = 'inputfile')
 	parser.add_argument("-o", "--output", type = str, default = "", help = "Output tsv file", dest = 'outputfile')
 	return parser.parse_args()
+
 
 if __name__ == "__main__":
 	doctest.testmod()
