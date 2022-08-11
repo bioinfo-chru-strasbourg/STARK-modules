4407f0ad4c194c818af7adfc4efe4675fbfbff9b
elyanes <rauchmateusz@gmail.com>
2022-08-11 15:42:51 +0200

    non redundant left

---
+++ services/variantannotation/varank/cli/dockerfile/bin/non_redundant_generator.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import glob
+from os.path import join as osj
+import os
+import subprocess
+import logging as log
+
+def generate(run_informations):
+    print(run_informations)
+
+
+if __name__ == "__main__":
+    pass
