d1dae14c78975ece1f9ab3b7507d13a9c79ee6d2
SamuelNicaise <ichtyornis@gmail.com>
2022-10-20 12:31:53 +0200

    Create module interfaces & switch edith to submodule (#2)
    * Fix for php-based module, using Apache-buster image, fix for multisampleanalysis and cqi, error without yum update
    
    * create interfaces, mv edith, create cloudgene
    
    * add stache pages in config
    
    * fix path Dockerfile, add CloudGene applications
    
    * chmod
    
    * add docker sock in docker-compose
    
    * remove edith module
    
    Co-authored-by: jean-baptiste lamouche <jbaptiste.lamouche@gmail.com>
    Co-authored-by: antonylebechec <antony.lebechec@gmail.com>
    Co-authored-by: root SOCLE_CENTOS7 <root@int0663.hus-integration.fr>

---
+++ services/interfaces/cloudgene/dashboard/dockerfile/scripts/launch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p /STARK/tools/cloudgene/current/bin/config/apps
+mkdir -p /STARK/tools/cloudgene/current/bin/data/tmp
+mkdir -p /STARK/tools/cloudgene/current/bin/data/workspace
+/STARK/tools/cloudgene/current/bin/cloudgene server
