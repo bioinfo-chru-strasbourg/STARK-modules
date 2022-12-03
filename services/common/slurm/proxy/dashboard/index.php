611953a71d2fcefb00650422dcd16ecd5e316723
antonylebechec <antony.lebechec@gmail.com>
2022-12-03 03:28:51 +0100

    fix and improve dashboard

--- services/common/slurm/proxy/dashboard/index.php
+++ services/common/slurm/proxy/dashboard/index.php
@@ -45,104 +45,118 @@ $jobs=$sacct["jobs"];
 
 krsort($jobs, $flags = SORT_REGULAR);
 
-// echo "<pre>";
+$cell_style = "";
+
+
+$nb = 0;
+
 foreach ($jobs as $job_id => $job_infos) {
-    $job_list[$job_id]['id'] = $job_infos['job_id'];
-    $job_list[$job_id]['name'] = $job_infos['name'];
-    $job_list[$job_id]['time'] = $job_infos['time']['elapsed'];
-	#echo $job_list[$job_id]['id']." ".$job_list[$job_id]['name']." ".$job_list[$job_id]['time']."<br>";
-	#$job_infos["tres"];
+
+	# Time
 	$seconds = $job_infos['time']['elapsed'];
 	$time=$output = sprintf('%02d:%02d:%02d', ($seconds/ 3600),($seconds/ 60 % 60), $seconds% 60);
-	#print_r($job_infos['steps'][0]["tres"]["allocated"]);
+
+	# Requested
+	$requested = "";
+	foreach ($job_infos["tres"]["requested"] as $key => $value) {
+		if ($value["type"] == "cpu") {
+			$requested .= $value["count"]."CPUs ";
+		};
+		if ($value["type"] == "mem") {
+			$requested .= round($value["count"]/1024)."Go ";
+		};
+	};
+
+	# Allocated
 	$allocated = "";
-	foreach ($job_infos['steps'][0]["tres"]["allocated"] as $key => $value) {
+	foreach ($job_infos["tres"]["allocated"] as $key => $value) {
 		if ($value["type"] == "cpu") {
 			$allocated .= $value["count"]."CPUs ";
 		};
 		if ($value["type"] == "mem") {
 			$allocated .= round($value["count"]/1024)."Go ";
 		};
-		#print($value["type"]."=".$value["count"]);
-		// if ($value["count"] != "") {
-		// 	$allocated .= $value["type"]."=".$value["count"]." ";
-		// };
+	};
+	if ($allocated == "") {
+		$allocated = "-";
+	}
+
+	# Time Start
+	if ($job_infos['time']['start']) {
+		$start = date("Y/m/d H:m:s",$job_infos['time']['start']);
+	} else {
+		$start = "";
+	};
+	
+	# Time End
+	if ($job_infos['time']['end']) {
+		$end = date("Y/m/d H:m:s",$job_infos['time']['end']);
+	} else {
+		$end = "";
 	};
 
-	$status = $job_infos['exit_code']['status'];
-	// if ( $job_infos['exit_code']['return_code'] > 0 ) {
-	// 	$status .= "[".$job_infos['exit_code']['return_code']."]";
-	// }
+	# State
+	$state = $job_infos['state']['current'];
 
+	# Name
+	$name = $job_infos['name'];
+
+	# Row
 	$dataObject_array[]="
 			{
 				id: '".$job_infos['job_id']."',
-				name: '".$job_infos['name']."',
-				state: '".$job_infos['state']['current']."',
-				status: '".$status."',
+				name: '".$name."',
+				state: '".$state."',
 				
 				account: '".$job_infos['account']."',
 				qos: '".$job_infos['qos']."',
 				priority: '".$job_infos['priority']."',
 				time: '".$time."',
-				allocated: '".$allocated."'
+				start: '".$start."',
+				end: '".$end."',
+				allocated: '".$requested." / ".$allocated."'
 			}
 
 		";
-		# account: '".$job_infos['account']."',
-		#alloc: '".$job_infos['required']['CPUs']." CPUs / ".round($job_infos['required']['memory']/1024)."Go'
+
+	# Cell Style Renderer
+
+	# ID renderer
+	$cell_style .= "{row: ".$nb.", col: 0, renderer: 'GRAY_StylesRenderer'},";
+
+	# State renderer
+	$cell_style .= "{row: ".$nb.", col: 1, renderer: '".$state."_StylesRenderer'},";
+
+	# Name renderer
+	$cell_style .= "{row: ".$nb.", col: 2, renderer: 'BOLD_StylesRenderer'},";
+
+	$nb++;
 
 };
+
+# Implode Row
 $dataObject_jobs=implode(",", $dataObject_array);
-
-$columns_fields_list="
-	{ data: 'id', title: 'ID', type: 'numeric', readOnly: true, width: 10 },
-	{ data: 'name', title: 'Name', type: 'text', readOnly: true, width: 100, wordWrap: true, noWordWrapClassName: 'is-noWrapCell' },
-	{ data: 'state', title: 'State', type: 'text', readOnly: true, width: 20 },
-	
-	{ data: 'account', title: 'Account', type: 'text', readOnly: true, width: 20 },
-	{ data: 'qos', title: 'Quality Of Service', type: 'text', readOnly: true, width: 30 },
-	{ data: 'priority', title: 'Priority', type: 'text', readOnly: true, width: 15 },
-	{ data: 'time', title: 'Time Elapsed', type: 'text', readOnly: true, width: 25 },
-	{ data: 'allocated', title: 'Resources Allocated', type: 'text', readOnly: true }
-
-	";
-	# 	{ data: 'status', title: 'Status', type: 'text', readOnly: true, width: 30 },
-
-
-	#{ data: 'account', title: 'Job Account', type: 'text', readOnly: true, width: 30 },
-	#{ data: 'alloc', title: 'Job Resources Allocation', type: 'text', readOnly: true, width: 50 },
-$colHeaders_fields_list=" 'ID', 'Name', 'State', 'Account', 'QOS', 'Priority', 'Time', 'Allocated' ";
-
-$fixedColumnsLeft=2;
-
 $dataObject="
 var dataObject = [
- $dataObject_jobs
+	$dataObject_jobs
 ];
 ";
 
+# Columns
 $columns="[
-    $columns_fields_list
+	{ data: 'id', title: 'ID', type: 'numeric', readOnly: true, width: 50 },
+	{ data: 'state', title: 'State', type: 'text', readOnly: true, width: 100, renderer: 'html' },
+	{ data: 'name', title: 'Name', type: 'text', readOnly: true, width: 620, wordWrap: true },
+	{ data: 'account', title: 'Account', type: 'text', readOnly: true, width: 80 },
+	{ data: 'qos', title: 'QOS', type: 'text', readOnly: true, width: 80 },
+	{ data: 'priority', title: 'Priority', type: 'text', readOnly: true, width: 80 },
+	{ data: 'time', title: 'Time', type: 'text', readOnly: true, width: 70 },
+	{ data: 'start', title: 'Start', type: 'text', readOnly: true, width: 130 },
+	{ data: 'end', title: 'End', type: 'text', readOnly: true, width: 130 },
+	{ data: 'allocated', title: 'Requested / Allocated', type: 'text', readOnly: true }
 ]";
-
-
-$colHeaders="[
-    $colHeaders_fields_list
-]";
-
-// print_r($dataObject_array);
-// print_r($dataObject_jobs);
-# print_r($dataObject);
-
-// echo "</pre>";
-
-
-
-
-// echo "<pre>";
-// print_r($jobs);
-// echo "</pre>";
+$colHeaders="[ 'State', 'ID', 'Name', 'Account', 'QOS', 'Priority', 'Time', 'Start', 'End', 'Resources' ]";
+$fixedColumnsLeft=2;
 
 
 ?>
@@ -151,10 +165,8 @@ $colHeaders="[
 
 	<head>
 		<title></title>
-
 		<link rel="stylesheet" type="text/css" href="handsontable/dist/handsontable.full.min.css">
 		<link rel="stylesheet" type="text/css" href="handsontable/static/css/main.css">
-
 		<script src="handsontable/dist/handsontable.full.min.js"></script>
 	</head>
 
@@ -166,7 +178,55 @@ $colHeaders="[
 
 			<?php print $dataObject; ?>
 
-			var currencyCodes = ['EUR', 'EURR', 'JPY', 'GBP', 'CHF', 'CAD', 'AUD', 'NZD', 'SEK', 'NOK', 'BRL', 'CNY', 'RUB', 'INR', 'TRY', 'THB', 'IDR', 'MYR', 'MXN', 'ARS', 'DKK', 'ILS', 'PHP'];
+			Handsontable.renderers.registerRenderer('COMPLETED_StylesRenderer', (hotInstance, TD, ...rest) => {
+				Handsontable.renderers.TextRenderer(hotInstance, TD, ...rest);
+
+				TD.style.fontWeight = 'bold';
+				TD.style.color = 'white';
+				TD.style.background = 'green';
+			});
+
+			Handsontable.renderers.registerRenderer('RUNNING_StylesRenderer', (hotInstance, TD, ...rest) => {
+				Handsontable.renderers.TextRenderer(hotInstance, TD, ...rest);
+
+				TD.style.fontWeight = 'bold';
+				TD.style.color = 'white';
+				TD.style.background = 'blue';
+			});
+
+			Handsontable.renderers.registerRenderer('PENDING_StylesRenderer', (hotInstance, TD, ...rest) => {
+				Handsontable.renderers.TextRenderer(hotInstance, TD, ...rest);
+
+				TD.style.fontWeight = 'bold';
+				TD.style.color = 'white';
+				TD.style.background = 'gray';
+			});
+
+			Handsontable.renderers.registerRenderer('FAILED_StylesRenderer', (hotInstance, TD, ...rest) => {
+				Handsontable.renderers.TextRenderer(hotInstance, TD, ...rest);
+
+				TD.style.fontWeight = 'bold';
+				TD.style.color = 'white';
+				TD.style.background = 'red';
+			});
+
+			Handsontable.renderers.registerRenderer('BOLD_StylesRenderer', (hotInstance, TD, ...rest) => {
+				Handsontable.renderers.TextRenderer(hotInstance, TD, ...rest);
+
+				TD.style.fontWeight = 'bold';
+				// TD.style.color = 'white';
+				// TD.style.background = 'red';
+			});
+
+			Handsontable.renderers.registerRenderer('GRAY_StylesRenderer', (hotInstance, TD, ...rest) => {
+				Handsontable.renderers.TextRenderer(hotInstance, TD, ...rest);
+
+				TD.style.fontWeight = 'bold';
+				TD.style.color = 'white';
+				TD.style.background = 'gray';
+			});
+
+
 			var flagRenderer = function (instance, td, row, col, prop, value, cellProperties) {
 			var currencyCode = value;
 			while (td.firstChild) {
@@ -186,23 +246,23 @@ $colHeaders="[
 			var hotElementContainer = hotElement.parentNode;
 			var hotSettings = {
 			data: dataObject,
-			fillHandle: true,
+			fillHandle: false,
 			//overflow: auto,
 			//autoRowSize: {syncLimit: 10},
 			//RowSize: 10,
-			trimRows: true,
+			trimRows: false,
 			columns: <?php print $columns ?>,
-			stretchH: 'all',
+			stretchH: 'last',
 			//width: '800',
 			colWidths: 100,
 			autoWrapCol: true,
-			autoWrapRow: true,
-			autoRowSize: true,
+			autoWrapRow: false,
+			autoRowSize: false,
 			height: '100%',
 			//height: 800,
 			maxRows: 2000,
 			rowHeaders: true,
-			rowHeights: '10px',
+			rowHeights: '1px',
 			search: true,
 			colHeaders: <?php echo $colHeaders; ?>,
 			mergeCells: true,
@@ -238,32 +298,15 @@ $colHeaders="[
 			// },
 			exportFile: true,
 			// trimRows: [
-			// 	0,
-			// 	0,
-			// 	0,
+			// 	100,
 			// 	0
 			// ],
+			cell: [ <?php echo $cell_style; ?>],
 			licenseKey: 'non-commercial-and-evaluation'
 			};
 			var hot = new Handsontable(hotElement, hotSettings);
-			// document.getElementById("export-tsv").addEventListener("click", function(event) { hot.getPlugin("exportFile").downloadFile("csv", {
-			// 	filename: "<?php echo $t_filename; ?>_export_[YYYY][MM][DD]",
-			// 	fileExtension: "tsv",
-			// 	columnDelimiter: "\t",
-			// 	exportHiddenColumns: true,
-			// 	columnHeaders: true
-			// });})
-			// document.getElementById("export-string").addEventListener("click", function(event) {console.log(hot.getPlugin("exportFile").exportAsString("csv"));})
-
 
 		</script>
-
-
-
-
-		
-		
-
 
 	</body>
 
