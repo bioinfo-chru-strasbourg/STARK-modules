3c8e7b1ffe6853af337c14bd831513985b911942
lavauxt <lavauxt@github.com>
2024-03-12 09:46:12 +0100

    CANOES NextGen

--- services/structuralvariation/canoes/app/scripts/merge_multicov.py
+++ services/structuralvariation/canoes/app/scripts/merge_multicov.py
@@ -1,49 +1,38 @@
 import argparse
-import pandas as pd
 
-def generate_multicov_output(input_files, output_file):
-    # Initialize an empty DataFrame
-    df = pd.DataFrame()
-
-    # Define the header
-    header = ["chrom", "start", "end"]
-
-    # Loop through input files
-    for multicovfile in input_files:
-        sample_name = multicovfile.split("/")[-1].split(".")[0]
-        header.append(sample_name)
-
-        # Read data from file and append to DataFrame
-        with open(multicovfile, "r") as file:
-            for line in file:
-                fields = line.strip().split("\t")
-                index = "\t".join(fields[:3])
-                value = fields[-1]
-
-                # Check if index already exists in DataFrame
-                if index not in df.index:
-                    df.loc[index] = [None] * len(header)
-                df.at[index, sample_name] = value
-
-    # Write DataFrame to output file
-    df.columns = header
-    df.to_csv(output_file, sep='\t', index=False)
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Process multicov files.")
+    parser.add_argument("input", nargs="+", help="Input multicov files")
+    parser.add_argument("-o", "--output", required=True, help="Output file")
+    return parser.parse_args()
 
 def main():
-    parser = argparse.ArgumentParser(description='Generate multicov output')
-    parser.add_argument('input_files', nargs='+', help='Input multicov files')
-    parser.add_argument('-o', '--output', help='Output file')
-    args = parser.parse_args()
+    args = parse_arguments()
+    
+    data = {}
+    header = ["chrom", "start", "end"]
+    
+    for multicovfile in args.input:
+        sample_name = multicovfile.split("/")[-1].split(".")[2]
+        header.append(sample_name)
+        
+        with open(multicovfile, "r") as i:
+            for line in i:
+                field = line.strip().split("\t")
+                index = "\t".join(field[0:3])
+                value = field[-1]
+                
+                if index not in data:
+                    data[index] = {}
+                data[index][sample_name] = value
 
-    if not args.input_files:
-        print("Error: At least one input file is required.")
-        return
-
-    if not args.output:
-        print("Error: Output file path is required.")
-        return
-
-    generate_multicov_output(args.input_files, args.output)
+    with open(args.output, "w") as o:
+        o.write("\t".join(header) + "\n")
+        for index in data:
+            sample_value = [index]
+            for sample_name in header[3:]:
+                sample_value.append(data[index].get(sample_name, "NA"))
+            o.write("\t".join(sample_value) + "\n")
 
 if __name__ == "__main__":
     main()
